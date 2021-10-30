92da5fdc2844a4fd3ee71c9ad68963850d960794
nimrod <nimrodwalwe@gmail.com>
2021-10-30 16:58:01 +0300

    bug fix on image path

--- src/sections_wrapper.js
+++ src/sections_wrapper.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
 import React from "react"
 import owner from "./images/owner.jpg"
